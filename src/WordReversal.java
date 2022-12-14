b7c0ec9fc4f1d7abdbd5975630d910c31db347da
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-12-14 17:06:32 +0100

    ciagle z bledem

--- src/WordReversal.java
+++ src/WordReversal.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class WordReversal {
     public static void main(String[] args) {
-        String sentence1[] = {};
+        String[] sentence1 = new String[5];
         System.out.println("Podaj pierwsze słowo: ");
         Scanner scan = new Scanner(System.in);
 
         for(int i = 0; i < 6; i++){
             String slowo = scan.nextLine();
             sentence1[i] = slowo;
-            i++;
+            System.out.println("Podaj kolejne slowo");
         }
-        for (int j = 0; j <= sentence1.length; j++) {
+        System.out.println(sentence1[3]);
+        for (int j = 0; j < sentence1.length; j++) {
            System.out.println(sentence1[j]);
         }
     }
